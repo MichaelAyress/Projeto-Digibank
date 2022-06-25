eefaf430e8b9e919ba994ecc73689b4cce9cea1e
Michael Ayres <99827730+MichaelAyress@users.noreply.github.com>
2022-06-25 17:38:03 -0300

    1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$("i").click(function () {
+    $("ul").toggleClass("open");
+  });
+  //cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+
+$("#menu-servico").click(function () {
+  $(".menu-2").toggleClass("menu-2-open");
+})
